9d9bfb767fb3878057f0a7374c152a5523d4c5f0
asis-parajuli <60426026+asis-parajuli@users.noreply.github.com>
2020-08-29 13:49:04 +0545

    Updates

---
+++ precache-manifest.5cc1561bea84d0324f953984f015f679.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6cfe7c0d1c6307beb09e9d1d7baaff8",
+    "url": "/cartoon-album/index.html"
+  },
+  {
+    "revision": "85b5e60deabebff42098",
+    "url": "/cartoon-album/static/css/main.426dcfb5.chunk.css"
+  },
+  {
+    "revision": "f16b2915901d1da0cfe0",
+    "url": "/cartoon-album/static/js/2.42609e5e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cartoon-album/static/js/2.42609e5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85b5e60deabebff42098",
+    "url": "/cartoon-album/static/js/main.5acae485.chunk.js"
+  },
+  {
+    "revision": "8cd7a79eda4f67745868",
+    "url": "/cartoon-album/static/js/runtime-main.56b8bd01.js"
+  }
+]);
